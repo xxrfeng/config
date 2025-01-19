8fcb80173f30dc4a5e696551b6c5cde941fa63c2
xxrfeng <49137814+xxrfeng@users.noreply.github.com>
2025-01-19 21:53:36 +0800

    Update singbox.js

--- singbox.js
+++ singbox.js
@@ -35,7 +35,7 @@ config.outbounds.map(i => {
     i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
   }
   if (['home' 'home-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /test/i))
+    i.outbounds.push(...getTags(proxies, /tes/i))
   }
 })
 
